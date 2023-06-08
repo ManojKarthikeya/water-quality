64ee60503208892e91997cbc29024bed6d6ff657
manojkarthikeya <manojkarthikeya@gmail.com>
2023-06-08 12:27:37 +0530

    asdasd

---
+++ server/myapp/__init__.py
@@ -0,0 +1,23 @@
+import joblib
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+
+def create_app():
+    ext = joblib.load('ext.joblib')
+
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route("/")
+    def base():
+        return "<h1>Hello from server!</h1>"
+
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        data = request.get_json()
+        print(data)
+        print(ext.predict([data['ratios']])[0])
+        return f"{ext.predict([data['ratios']])[0]}"
+    
+    return app
